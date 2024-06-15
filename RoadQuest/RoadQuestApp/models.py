fe07083e5f2e38a0a35666422a98df39f89e5f42
turtlelyte <terrli0407@gmail.com>
2024-06-15 16:01:16 -0700

    map page

--- RoadQuest/RoadQuestApp/models.py
+++ RoadQuest/RoadQuestApp/models.py
@@ -4,9 +4,19 @@ from django.db import models
 class RouteItem(models.Model):
     start = models.CharField(max_length=200)
     end = models.CharField(max_length=200)
+    start_lat = models.FloatField(blank=True, null=True)
+    start_lng = models.FloatField(blank=True, null=True)
+    end_lat = models.FloatField(blank=True, null=True)
+    end_lng = models.FloatField(blank=True, null=True)
 
     def __str__(self):
-        return f"Route from {self.start} to {self.end}"
+        return f"Route from {self.start} ({self.start_lat}, {self.start_lng}) to {self.end} ({self.end_lat}, {self.end_lng})"
+    
+    def get_start_coords(self):
+        return (self.start_lat, self.start_lng)
+    
+    def get_end_coords(self):
+        return (self.end_lat, self.end_lng)
     
 class POI(models.Model):
     name = models.CharField(max_length=255)
