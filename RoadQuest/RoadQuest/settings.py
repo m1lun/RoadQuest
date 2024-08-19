73562fa64179adcbbae08d565d45fc43d1051070
PJOESH <wangster0000@gmail.com>
2024-08-18 23:03:30 -0700

    clear

--- RoadQuest/RoadQuest/settings.py
+++ RoadQuest/RoadQuest/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-p77!@8j#u9g_yyia-ncijw5j!c9m*grbh8!)pqcj#&&$e56a%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['roadquest.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
