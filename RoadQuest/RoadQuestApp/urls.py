1a6505daef645e5c11638d8754d8595e36bc755f
Milun Gracias-Taplay <44041180+m1lun@users.noreply.github.com>
2024-08-19 22:56:10 -0700

    finished multi-filter

--- RoadQuest/RoadQuestApp/urls.py
+++ RoadQuest/RoadQuestApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"), # when we go to "" calls the views function "home"
     path('mapping/<str:start1>/<str:end1>/', views.mapping, name='mapping'),
-    path('mapping/<str:start1>/<str:end1>/<str:poi_type>/', views.mapping, name='mapping_filtered'),
+    path('mapping/<str:start1>/<str:end1>/<str:poi_types>/<str:poi_rating>/', views.mapping, name='mapping_filtered'),
     path("route/", views.route, name="route")
 ]
