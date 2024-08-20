217f16d41168cec784fb9e7b3dd37fe7a5f54c79
PJOESH <wangster0000@gmail.com>
2024-08-19 17:58:30 -0700

    root

--- RoadQuest/build.sh
+++ RoadQuest/build.sh
@@ -5,7 +5,7 @@ echo "Current directory: $(pwd)"
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip3 install -r RoadQuest/requirements.txt
+pip3 install -r requirements.txt
 
 # Convert static asset files
 python3 manage.py collectstatic --no-input
